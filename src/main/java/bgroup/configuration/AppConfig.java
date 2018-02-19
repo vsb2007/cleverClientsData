b0d75fa7af3de5b014c5764ca4784e129bc920f0
Vladimir Baranov <330034@gmail.com>
2018-02-19 23:37:38 +0600

    remove smsSender class

--- src/main/java/bgroup/configuration/AppConfig.java
+++ src/main/java/bgroup/configuration/AppConfig.java
@@ -1,6 +1,5 @@
 package bgroup.configuration;
 
-import bgroup.model.SmsSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
--- src/main/java/bgroup/service/UserServiceImpl.java
+++ src/main/java/bgroup/service/UserServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import bgroup.model.SmsSender;
 import bgroup.model.UserProfile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
